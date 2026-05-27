4462b52f6f5a0f2fb7b59ac5ccc37dadf0b01a6d
patadiya2106 <rahulpatadiya07@gmail.com>
2026-05-27 16:01:02 +0530

    CH-7

---
+++ CH-PRACTICE OF ch-7/if else.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("Enter your Age :");
+		scanf("%d",&a);	
+		
+	if(a>=18)
+	{
+		printf("You can VOTE.");
+	}
+	else
+	{
+		printf("You can't VOTE.");
+	}	
+}
