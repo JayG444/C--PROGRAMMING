40f79b73962b8bb7d570958beb55f0896f80c9ec
patadiya2106 <rahulpatadiya07@gmail.com>
2026-05-30 12:05:26 +0530

    Ternary Operator

---
+++ CH-7/L-3/P-1.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+main()
+{
+	int x,y,z;
+	printf("Enter the value of first Number :");
+	scanf("%d",&x);
+	printf("Enter the value of Second Number :");
+	scanf("%d",&y);
+	printf("Enter the value of Third Number :");
+	scanf("%d",&z);
+	
+	(x<y)?
+		(x<z)?
+		
+			printf("%d is Minimum.",x)	
+		
+		:
+		
+			printf("%d is Minimum.",z)
+		
+	
+	:
+	
+		(y<z)?
+		
+			printf("%d is Minimum.",y)	
+		
+		:
+		
+			printf("%d is Minimum.",z);
+		
+}
+
