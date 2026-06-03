32145864dc4c57a07d301f83d87a588d5529e381
patadiya2106 <rahulpatadiya07@gmail.com>
2026-06-03 11:52:11 +0530

    Grade Calculator

---
+++ CH-SIMPLE GRADE CALCULATOR/P-1.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+main()
+{
+	int Marks;
+	printf("Enter your score :");
+	scanf("%d",&Marks);
+	
+	(Marks>=90 && Marks<=100)
+	?printf("Your grade is A.Excellent Work!Congratulations!You are eligible for next level.")
+	:(Marks>=80 && Marks<90)
+	?printf("Your grade is B.Well Done!Congratulations!You are eligible for next level.")
+	:(Marks>=70 && Marks<80)
+	?printf("Your grade is C.Good Work!Congratulations!You are eligible for next level.")
+	:(Marks>=60 && Marks<70)
+	?printf("Your grade is D1.Good Effort.Congratulations!You are eligible for next level.")
+	:(Marks>=50 && Marks<60)
+	?printf("Your grade is D2.You Passed but you could do better!Congratulations!You are eligible for next level.")
+	:(Marks>=33 && Marks<50)
+	?printf("Your grade is E.Good but try harder.Congratulations!.You are eligible for next level.")
+	:(Marks>=0 && Marks<33)
+	?printf("Your grade is F.You Fail.Better luck, Next time.You are not eligible for next level.")
+				
+	: printf("Invalid Marks");
+}
