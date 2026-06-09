389446151d991b0e3989b792bb31f26eff4565be
JayG444 <rahulpatadiya07@gmail.com>
2026-06-09 12:32:21 +0530

    Looping

--- CH-LOOPING/L-2/Fibonacci for loop.c
+++ CH-LOOPING/L-2/Fibonacci for loop.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 main()
 {
-	int n,a=0,b=1,c;
+	int i,n,a=0,b=1,c;
 	printf("Enter your Number :");
 	scanf("%d",&n);
-	for(a=0;a<=n;a++)
+	printf("%d\t%d\t",a,b);
+	for(i=3;i<=n;i++)
+	{
 		c=a+b;
+		printf("%d\t",c);
 		a=b;
 		b=c;
-	printf("%d\t",c);
+	}	
 }
