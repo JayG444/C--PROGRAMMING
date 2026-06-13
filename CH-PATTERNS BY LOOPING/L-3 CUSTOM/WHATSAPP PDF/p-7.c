f23976017cbfce214c0030e7f1930b35afeef58d
AdarashDubey <adarshdubey2708@gmail.com>
2026-06-13 14:55:25 +0530

    Patterns

---
+++ CH-PATTERNS BY LOOPING/L-3 CUSTOM/WHATSAPP PDF/p-7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main()
+{
+	int i;
+	
+	for(i=1; i<=7; i++)
+	{
+		if(i==1 || i==7 || i==4)
+			printf("* * *\n");
+		else if(i==2 || i==6)
+			printf("*     *\n");
+		else 
+			printf("*    *\n");
+	}
+}
