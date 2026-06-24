66c950aaf31c9ea6f3d2644cad16c4fa889001ba
AdarashDubey <adarshdubey2708@gmail.com>
2026-06-24 15:29:20 +0530

    String

---
+++ CH-STRING/L-1/P-2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+    char name[50];
+    printf("Enter your Name :");
+    scanf("%[^\n]",&name);
+
+    for (int i = 0; name[i]!=0 ; i++)
+    {
+        if(name[i]>='a' && name[i]<='z')
+       {
+         name[i]-=32;
+       }
+    }
+    printf("UPPERCASE of your Name is :%s",name);
+}
---
+++ CH-STRING/L-1/code.c
@@ -0,0 +1,35 @@
+{
+  "editor.inlineSuggest.enabled": true,
+
+  "editor.quickSuggestions": {
+    
+
+    "other": true,
+    "comments": true,
+    "strings": true
+  },
+
+  "editor.suggestOnTriggerCharacters": true,
+  "editor.acceptSuggestionOnEnter": "on",
+  "editor.suggestSelection": "first",
+  "editor.tabCompletion": "on",
+
+  "editor.parameterHints.enabled": true,
+
+  "editor.wordBasedSuggestions": "currentDocument",
+
+  "editor.snippetSuggestions": "top",
+
+  "C_Cpp.intelliSenseEngine": "default",
+  "C_Cpp.autocomplete": "Default",
+
+  // ⭐ THIS IS THE KEY FOR "LIKE IMAGE" EXPERIENCE
+  "clangd.arguments": [
+    "--background-index",
+    "--suggest-missing-includes",
+    "--clang-tidy"
+  ],
+
+"clangd.path": "clangd",
+"code-runner.runInTerminal": true
+}
