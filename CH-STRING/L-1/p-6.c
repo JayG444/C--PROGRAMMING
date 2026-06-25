c344ded0591364f1e16fcb6e6f247d48f8c49231
AdarashDubey <adarshdubey2708@gmail.com>
2026-06-25 15:44:42 +0530

    String

--- CH-STRING/L-1/p-6.c
+++ CH-STRING/L-1/p-6.c
@@ -1,1 +1,21 @@
-#
+#include <stdio.h>
+main()
+{
+    char name[50];
+    printf("Enter your Name: ");
+    scanf("%[^\n]", &name);
+
+    if (name[0] >= 'a' && name[0] <= 'z')
+    {
+         name[0] -= 32;
+    }
+
+    for (int i = 1; name[i] != '\0'; i++)
+    {
+        if (name[i - 1] == ' ' && name[i] >= 'a' && name[i] <= 'z')
+        {
+            name[i] -= 32;
+        }
+    }
+    printf("Title Case of your Name is : %s", name);
+}
