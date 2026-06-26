47151c68a7c21301d41adf441aee2d75d54b06c9
AdarashDubey <adarshdubey2708@gmail.com>
2026-06-26 13:46:04 +0530

    Filament

--- CH-FILAMENT/Q-1.C
+++ CH-FILAMENT/Q-1.C
@@ -1,13 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 main()
 {
-	char str[50];
-    int i,j;
-	printf("Enter any String:");
-	scanf("%[^\n]",&str);
-	
-
-	(str[i]==str[j])
-	?printf("Your given String is a Palindrome String.")
-	:printf("Your given String is not a Palindrome String.");	
+    char str[50];
+    int i, j, z = 1;
+    printf("Enter any String: ");
+    scanf("%[^\n]", str);
+    for (i = 0; str[i] != '\0'; i++)
+    {
+    }
+    for (j = 0; j < i; j++)
+    {
+        if (str[j] != str[i - j - 1])
+        {
+            z = 0;
+            break;
+        }
+    }
+    if (z == 1)
+        printf("The given string is Palindrome.");
+    else
+        printf("The given string is not Palindrome.");
 }
