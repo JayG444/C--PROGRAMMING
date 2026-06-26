8c7d4caf51bd51ccbbf6c0935fa13d9edb3c75e9
AdarashDubey <adarshdubey2708@gmail.com>
2026-06-26 12:35:20 +0530

    Filament

---
+++ CH-FILAMENT/Q-1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	char str[50];
+    int i,j;
+	printf("Enter any String:");
+	scanf("%[^\n]",&str);
+	
+
+	(str[i]==str[j])
+	?printf("Your given String is a Palindrome String.")
+	:printf("Your given String is not a Palindrome String.");	
+}
