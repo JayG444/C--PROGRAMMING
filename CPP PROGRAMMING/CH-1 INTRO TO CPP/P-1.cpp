fdbf34da8baec18181c6693718b88526cc475190
AdarashDubey <adarshdubey2708@gmail.com>
2026-07-14 15:16:00 +0530

    INTRO TO CPP

--- CPP PROGRAMMING/CH-1 INTRO TO CPP/P-1.cpp
+++ CPP PROGRAMMING/CH-1 INTRO TO CPP/P-1.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    std::cout << "Hello World...\n";
-    std::cout << "Hello C++ programming...\n";
+    std::cout << "Name: Jay Gohil\n";
+    std::cout << "Currently Learning C++\n";
     return 0;
 }
