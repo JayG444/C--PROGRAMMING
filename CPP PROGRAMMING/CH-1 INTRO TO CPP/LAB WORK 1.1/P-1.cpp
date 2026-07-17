eca1d7a3e9d3f494b3bcf1971f092fc2458f92cc
AdarashDubey <adarshdubey2708@gmail.com>
2026-07-17 13:18:53 +0530

    INTRO TO CPP

---
+++ CPP PROGRAMMING/CH-1 INTRO TO CPP/LAB WORK 1.1/P-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter Array size :";
+    cin >> size;
+
+    int arr[size];
+
+    cout << "Enter Array Elements :" << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << " a" << "[" << i << "]" << " = ";
+        cin >> arr[i];
+    }
+
+    cout << "Even Elements of an Array :"<<endl;
+
+    
+
+}
