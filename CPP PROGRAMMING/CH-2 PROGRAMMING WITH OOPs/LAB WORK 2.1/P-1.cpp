6ce87d3b446779e1b2ccff27cb806d0eb3bb601c
AdarashDubey <adarshdubey2708@gmail.com>
2026-07-18 13:11:26 +0530

    PROGRAMMING WITH OOP

--- CPP PROGRAMMING/CH-2 PROGRAMMING WITH OOPs/LAB WORK 2.1/P-1.cpp
+++ CPP PROGRAMMING/CH-2 PROGRAMMING WITH OOPs/LAB WORK 2.1/P-1.cpp
@@ -4,47 +4,51 @@ using namespace std;
 
 class Employee
 {
-public:
-    int emp_id;
-    char emp_name[50];
-    int emp_age;
-    char emp_role[50];
-    double emp_salary;
-    char emp_city[50];
-    int emp_experience;
-    char emp_company_name[50];
+    public:
+        int emp_id;
+        char emp_name[50];
+        int emp_age;
+        char emp_role[50];
+        float emp_salary;
+        char emp_city[50];
+        int emp_experience;
+        char emp_company_name[50];
 };
 
 int main()
 {
     Employee e[5];
 
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < 5; i++)
     {
-        cout << "Employee Number :" << i+1 << endl;
+        cout << "Employee Number :" << i + 1 << endl;
 
-        cout << "Enter Employee ID: ";
+        cout << "Enter Employee ID : ";
         cin >> e[i].emp_id;
 
-        cout << "Enter Name: ";
+        fflush(stdin);
+        cout << "Enter Name : ";
         gets(e[i].emp_name);
 
-        cout << "Enter Age: ";
+        cout << "Enter Age : ";
         cin >> e[i].emp_age;
 
-        cout << "Enter Role: ";
+        fflush(stdin);
+        cout << "Enter Role : ";
         gets(e[i].emp_role);
 
-        cout << "Enter Salary: ";
+        cout << "Enter Salary : ";
         cin >> e[i].emp_salary;
 
-        cout << "Enter City: ";
+        fflush(stdin);
+        cout << "Enter City : ";
         gets(e[i].emp_city);
 
-        cout << "Enter Company Name: ";
+        fflush(stdin);
+        cout << "Enter Company Name : ";
         gets(e[i].emp_company_name);
 
-        cout << "Enter Experience (in years): ";
+        cout << "Enter Experience (in years) : ";
         cin >> e[i].emp_experience;
 
         cout << endl;
@@ -52,13 +56,13 @@ int main()
 
     for (int i = 0; i < 5; i++)
     {
-        cout << "ID: " << e[i].emp_id
-             << "Name: " << e[i].emp_name
-             << "Age: " << e[i].emp_age
-             << "Role: " << e[i].emp_role
-             << "Salary: " << e[i].emp_salary
-             << "City: " << e[i].emp_city
-             << "Experience: " << e[i].emp_experience << " years"
-             << "Company: " << e[i].emp_company_name << endl;
+        cout << "ID : " << e[i].emp_id <<" || "
+             << "Name : " << e[i].emp_name << " || "
+             << "Age : " << e[i].emp_age <<" || "
+             << "Role : " << e[i].emp_role <<" || "
+             << "Salary : " << e[i].emp_salary <<" || "
+             << "City : " << e[i].emp_city <<" || "
+             << "Experience : " << e[i].emp_experience << " years" <<" || "
+             << "Company : " << e[i].emp_company_name << endl << endl;
     }
 }
